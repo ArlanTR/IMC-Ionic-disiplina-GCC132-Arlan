ee7af00058a9a7fe8c6e7875523b0ba6ab93a0d6
Paulo Jr <pauloa.junior@ufla.br>
2022-12-01 20:04:51 -0300

    Efetuando e apresentando o cálculo do IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,16 +8,22 @@ import { ToastController } from '@ionic/angular';
 })
 export class HomePage {
 
+  weight: number = 0
+  height: number = 0
+  imc: number = 0
+
   constructor(private toastCtrl: ToastController) {}
 
   onCalculate() {
-    this.showMessage()
+    this.imc = this.weight / (this.height * this.height)
+    this.showIMC()
   }
 
-  async showMessage() {
+  async showIMC() {
     const toast = await this.toastCtrl.create({
-      message: 'clicou',
+      message: `IMC = ${this.imc.toFixed(2)}`,
       duration: 3000,
+      position: 'top'
     })
     
     toast.present()
